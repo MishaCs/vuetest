6dfeb06109bf4c4d7aa6c2e21dd5c15eb24de6b4
MishaIsm <mismajloff@gmail.com>
2020-11-11 17:55:44 +0300

    wip

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -10,12 +10,27 @@
                     :class="{invalid:$v.email.$dirty && !$v.email.email}">
                     
                 <label for="email">Email</label>
-                <small class="helper-text invalid">Email</small>
+                <small 
+                    v-if="$v.email.$dirty && !$v.email.required"
+                    class="helper-text invalid">Поле email не должно быть пустым
+                </small>
+                <small 
+                    v-else-if="$v.email.$dirty && !$v.email.email"
+                    class="helper-text invalid">Введите корректный email
+                </small>
             </div>
             <div class="input-field">
-                <input id="password" type="password" class="validate">
+                <input
+                    id="password" 
+                    type="password" 
+                    v-model.trim="password"
+                    :class="{invalid:$v.password.$dirty && !$v.password.required}">
                 <label for="password">Пароль</label>
-                <small class="helper-text invalid">Password</small>
+                <small 
+                    class="helper-text invalid"
+                    v-if="$v.password.$dirty && !$v.password.required">
+                    Введите пароль
+                </small>
             </div>
         </div>
         <div class="card-action">
@@ -53,9 +68,15 @@ export default {
             if(this.$v.$invalid) {
                 this.$v.$touch()
                 return
-
             }
-            this.$router.puh('/')
+
+            const formData = {
+                email: this.email,
+                password: this.password
+            }
+
+            console.log(formData)
+            this.$router.push('/')
         }
     }
 }
